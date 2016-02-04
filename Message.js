22472ebacbc68018b8f8f702c7ec225bf3ef554c
Chris Kwan <chriswkwan@gmail.com>
2016-02-04 07:53:21 -0500

    Create message objects for data viz from gmail api responses

---
+++ Message.js
@@ -0,0 +1,13 @@
+var Message = function() {
+    return {
+        id: null,
+        sender: null,
+        senderIPAddress: null,
+        isRead: false,
+        isStarred: false,
+        date: null,
+        dayOfWeek: null,
+        location: null,
+        snippet: null
+    }
+};
