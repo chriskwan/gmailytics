95f38515d88e72c6b83677bbf15d6b178c8304f6
Chris Kwan <chriswkwan@gmail.com>
2016-02-05 23:16:57 -0500

    Sort the bars in the emails by sender graph

--- dashboard.js
+++ dashboard.js
@@ -64,6 +64,18 @@ function makeEmailsBySenderChart() {
         }
     }
 
+    var reverseCompare = function(a,b) {
+        if (a.count < b.count) {
+            return 1;
+        }
+        if (a.count > b.count) {
+            return -1;
+        }
+        return 0;
+    };
+
+    data.sort(reverseCompare);
+
     dataForEmailsBySender = data; //cwkTODO global bad I know
 }
 
